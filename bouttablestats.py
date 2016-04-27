2478d7f141d7f3d1c8594f940fb99ff4895b4f25
Tom Kazimiers <tom@voodoo-arts.net>
2016-04-27 18:07:33 -0400

    Add "-bf" option to write (merged) bouts to a CSV file

--- bouttablestats.py
+++ bouttablestats.py
@@ -24,7 +24,7 @@ from itertools import groupby
 logger = logging.getLogger(__name__)
 
 def create_histograms(s, p, path, offset=1, begin_with_stimuli=False,
-        delim=';', nheadrows=1, nomerge=False, max_rows=None):
+        delim=';', nheadrows=1, nomerge=False, max_rows=None, bout_file=None):
 
     logger.info("Creating pattern histograms beginning with {} bout in file: {}" \
         .format('stimuli' if begin_with_stimuli else 'pause', path))
@@ -34,11 +34,13 @@ def create_histograms(s, p, path, offset=1, begin_with_stimuli=False,
     head_rows = []
     raw_stimuli = []
     raw_pauses = []
+    boutcsv = []
     with open(path, 'r') as csvfile:
         linereader = csv.reader(csvfile, delimiter=delim)
         stimuli_bout = begin_with_stimuli
         current_loc = offset
         bout_label = "Bout" if nomerge else "Bout (merged)"
+
         for n, row in enumerate(linereader):
             if n < nheadrows:
                 head_rows.append(row)
@@ -46,6 +48,14 @@ def create_histograms(s, p, path, offset=1, begin_with_stimuli=False,
             if max_rows and n + nheadrows >= max_rows:
                 logger.info("Reached row limit of {} rows".format(max_rows))
                 break
+
+            # Prepare bout copying, if enabled
+            if bout_file:
+                copy_target = []
+                boutcsv.append(copy_target)
+            else:
+                copy_target = None
+
             # Collect all bouts
             n_stimuli_bouts = 0
             n_pause_bouts = 0
@@ -56,7 +66,7 @@ def create_histograms(s, p, path, offset=1, begin_with_stimuli=False,
                 raw_bout = row[start:(start + length)]
 
                 # Merge adjacent bout elements if they are the same, if not disabled
-                bout = tuple(raw_bout if nomerge else [k for k,v in groupby(raw_bout)])
+                bout = raw_bout if nomerge else [k for k,v in groupby(raw_bout)]
                 bout_alias = "S" if stimuli_bout else "P"
                 logger.debug("{}: Bout start: {} bound end: {} {}: {}" \
                         .format(bout_alias, start, start + length - 1, bout_label, ",".join(bout)))
@@ -71,6 +81,10 @@ def create_histograms(s, p, path, offset=1, begin_with_stimuli=False,
                 else:
                     raw_pauses.append(bout)
                     n_pause_bouts += 1
+
+                if bout_file:
+                    bout_copy = bout if nomerge else pad_bout(bout, length)
+                    copy_target.append(bout_copy)
 
                 stimuli_bout = not stimuli_bout
 
@@ -89,6 +103,25 @@ def create_histograms(s, p, path, offset=1, begin_with_stimuli=False,
 
     logger.info("Stimuli histogram: \n{}".format(format_histogram(stimuli_ptree)))
     logger.info("Pause histogram: \n{}".format(format_histogram(pause_ptree)))
+
+    if bout_file:
+        logger.info("Writing bout CSV file")
+        # If wanted, write out the merged bouts, padded to match the original
+        # column counts.
+        with open(bout_file, 'w') as boutcsvfile:
+            for bouts in boutcsv:
+                columns = [v for b in bouts for v in b]
+                line = delim.join(columns)
+                boutcsvfile.write(line)
+                boutcsvfile.write("\n")
+
+def pad_bout(bout, length, pad_char="0"):
+    """Will add extra padding characters (default: 0) to the bout list if it is
+    shorter than the passed in length."""
+    ldiff = length - len(bout)
+    if ldiff > 0:
+        bout = bout + [pad_char] * ldiff
+    return bout
 
 def percentage_tree(histogram):
     def walk(nodes):
@@ -146,6 +179,8 @@ def get_arg_parser():
             help='Number of header rows in CSV file', default='info')
     parser.add_argument("-n", "--max-rows", type=int, default=None,
             help="Limit the number of rows to read fom the input file")
+    parser.add_argument("-bf", "--bout-file", type=str,
+            help='Write out bouts to a new CSV file', default=None)
     parser.add_argument("s", type=int, help="the number of stimuli columns")
     parser.add_argument("p", type=int, help="the number of pause columns")
     parser.add_argument("file", type=str, help="the CSV file to load")
@@ -164,4 +199,4 @@ if __name__ == '__main__':
 
     create_histograms(args.s, args.p, args.file, args.offset, not
             args.stimuli_first, args.delim, args.head_rows, args.no_merge,
-            args.max_rows)
+            args.max_rows, args.bout_file)
